000fec01b7d4047722ef2fa83bd59a505c26d389
RegularSalamander <47067447+RegularSalamander@users.noreply.github.com>
2021-09-23 15:17:29 -0500

    added player shooting

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -3,35 +3,48 @@ extends KinematicBody2D
 # speed in pixels per second
 export (int) var moveSpeed = 150
 
+var bullet_scn
+
 # the directional buttons the player is pressing
 var input = [0, 0, 0, 0]
+var shooting = false
 # keeps track of the direction the player is facing
 # (isn't useful yet, but we'll need it when the player has actual animations)
 var lastDir = 0
+var reload_time = 0
 
-# Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	bullet_scn = load("res://Scenes/PlayerBullet.tscn")
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _physics_process(_delta):
-	var motion = Vector2(int(input[3])-int(input[2]), int(input[1])-int(input[0]))
-	if motion.x > 0:
+func _physics_process(delta):
+	reload_time -= delta
+	
+	var velocity = Vector2(int(input[3])-int(input[2]), int(input[1])-int(input[0]))
+	if velocity.x > 0:
 		lastDir = 3
-	elif motion.x < 0:
+	elif velocity.x < 0:
 		lastDir = 1
-	elif motion.y < 0:
+	elif velocity.y < 0:
 		lastDir = 2
-	elif motion.y > 0:
+	elif velocity.y > 0:
 		lastDir = 0
-	motion *= moveSpeed
+	
+	if shooting and reload_time <= 0:
+		var new_bullet = bullet_scn.instance()
+		new_bullet.position = position
+		new_bullet.velocity = Vector2(0, -200)
+		get_parent().add_child(new_bullet)
+		# half second reload
+		reload_time = 0.5
+	
+	velocity *= moveSpeed
 	# move and slide uses delta internally for some fuckin reason
-	var _slidevel = move_and_slide(motion)
+	move_and_collide(velocity*delta)
 
 func _input(event):
 	# event.is_action works for wasd or arrows
-	if event.is_action("ui_up") and not input[0]:
+	if event.is_action_pressed("ui_up") and not input[0]:
 		input[0] = true
 	if event.is_action_released("ui_up"):
 		input[0] = false
@@ -47,4 +60,9 @@ func _input(event):
 		input[3] = true
 	if event.is_action_released("ui_right"):
 		input[3] = false
+	
+	if event.is_action_pressed("jump"):
+		shooting = true
+	if event.is_action_released("jump"):
+		shooting = false
 	pass
