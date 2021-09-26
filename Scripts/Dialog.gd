f989a6cde971739841bd9ffad283c49cc273a7df
Dakota Fisher <dakota.fisher@dakotafisher.net>
2021-09-26 21:38:32 +0000

    added portraits and text speed

--- Scripts/Dialog.gd
+++ Scripts/Dialog.gd
@@ -5,13 +5,18 @@ var dialogQueue : Array
 var dialogActive : bool
 
 var textNode : RichTextLabel
+var portraitNode : TextureRect
 
-var textFreq = 5
+var textPeriod : float = 0.01
+var textTime : float
+var textInd : int
+var text : String
 
 export (String, FILE) var initDialog : String = ""
 
 func _ready():
 	textNode = get_node("TextArea/DialogText")
+	portraitNode = get_node("TextArea/PortraitPicture")
 	visible = false
 	if initDialog != "":
 		start(initDialog)
@@ -21,6 +26,7 @@ func dialogClose():
 	# Engine.time_scale = 1
 	visible = false
 	dialogActive = false
+	portraitNode.texture = null
 	pass
 
 func dialogNext():
@@ -28,10 +34,23 @@ func dialogNext():
 		var next = dialogQueue.pop_front()
 		match typeof(next):
 			TYPE_STRING:
-				textNode.text = next
+				if textPeriod <= 0:
+					textNode.text = next
+					text = next
+				else:
+					textInd = 0
+					textNode.text = ""
+					text = next
+					textTime = textPeriod
 				return null
 			TYPE_ARRAY:
 				dialogQueue = next + dialogQueue
+			TYPE_DICTIONARY:
+				match next["type"]:
+					"set-portrait":
+						var texture : ImageTexture = ImageTexture.new()
+						texture.load("res://" + next["res"])
+						portraitNode.texture = texture
 	dialogClose()
 
 func start(file : String):
@@ -45,11 +64,20 @@ func startSpec(diaSpec):
 	# Engine.time_scale = 0
 	visible = true
 	dialogActive = true
+	portraitNode.texture = null
 	match typeof(diaSpec):
 		TYPE_DICTIONARY:
 			dialog = diaSpec
 			dialogQueue = [dialog["start"]]
 			dialogNext()
+
+func _process(delta):
+	if textInd < text.length():
+		textTime -= delta
+		while textTime < 0:
+			textTime += textPeriod
+			textInd += 1
+			textNode.text = text.left(textInd)
 
 func _input(event):
 	if dialogActive && event.is_action_pressed("ui_accept"):
