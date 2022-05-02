249c1b429667650882b7fe16fa9febbb632470fe
RegularSalamander <47067447+RegularSalamander@users.noreply.github.com>
2022-05-02 16:16:24 -0500

    arcade edition

--- Scenes/DifficultyButton.gd
+++ Scenes/DifficultyButton.gd
@@ -2,7 +2,13 @@ extends Button
 
 export (int) var difficulty = 1
 
+func _ready():
+	if difficulty == 0:
+		grab_focus()
+
 func _pressed():
+	if difficulty == -1:
+		get_tree().quit()
 	Global.difficulty = difficulty
 	Global.checkpoint = 1
 	get_tree().change_scene("res://Scenes/Game.tscn")
--- Scripts/ContinueButton.gd
+++ Scripts/ContinueButton.gd
@@ -2,5 +2,8 @@ extends Button
 
 export (int) var difficulty = 1
 
+func _ready():
+	grab_focus()
+
 func _pressed():
 	get_tree().change_scene("res://Scenes/Game.tscn")
